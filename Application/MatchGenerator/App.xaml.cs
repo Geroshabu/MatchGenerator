cfe160b259ed4e2a6fa9d58b60990ca49fbeabda
Geroshabu <oonstks@gmail.com>
2016-05-13 22:18:54 +0900

    Catch unhandled exceptions

--- Application/MatchGenerator/App.xaml.cs
+++ Application/MatchGenerator/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace MatchGenerator
 {
@@ -13,5 +14,16 @@ namespace MatchGenerator
 	/// </summary>
 	public partial class App : Application
 	{
+		public void Application_DispatchUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+		{
+			string message = string.Format(
+				"エラーが発生しました. バグかもしれないっす(´・Д・`)\n" +
+				"改善のためにも, エラー発生時の状況を\n" +
+				"教えてくれるとありがたいっすm(_ _)m\n\n" +
+				"(エラー内容 : {0} {1})",
+				e.Exception.GetType(), e.Exception.Message);
+			MessageBox.Show(message, "予期せぬエラー");
+			e.Handled = true;
+		}
 	}
 }
