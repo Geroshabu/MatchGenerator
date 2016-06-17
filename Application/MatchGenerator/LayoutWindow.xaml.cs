31a146373aa8b4093cba6dd5cca229411022593a
Geroshabu <oonstks@gmail.com>
2016-06-18 00:59:21 +0900

    Generate every court views

--- Application/MatchGenerator/LayoutWindow.xaml.cs
+++ Application/MatchGenerator/LayoutWindow.xaml.cs
@@ -33,16 +33,35 @@ namespace MatchGenerator
 
 			if (matches.Count != Layout.CourtCount)
 			{
-				throw new ArgumentOutOfRangeException("試合の数と, レイアウトのコート数が一致しませんです. ハイ.");
+				throw new ArgumentOutOfRangeException("matches", "試合の数と, レイアウトのコート数が一致しませんです. ハイ.");
 			}
 		}
 
 		private void Window_Loaded(object sender, RoutedEventArgs e)
 		{
-			CourtView court1 = new CourtView(Matches[0]);
-			Viewbox viewBox1 = new Viewbox();
-			layoutGrid.Children.Add(viewBox1);
-			viewBox1.Child = court1;
+			double court_width_with_margin = ActualWidth / Layout.Column;
+			double court_height_with_margin = ActualHeight / Layout.Row;
+			double margin_width = 10;
+			double margin_height = 10;
+			double court_width = court_width_with_margin - 2 * margin_width;
+			double court_height = court_height_with_margin - 2 * margin_height;
+
+			for (int r = 0; r < Layout.Row; r++)
+			{
+				for (int c = 0; c < Layout.Column; c++)
+				{
+					CourtView court = new CourtView(Matches[r * Layout.Column + c]);
+					Viewbox viewBox = new Viewbox();
+					viewBox.Child = court;
+
+					viewBox.Margin = new Thickness(
+						c * court_width_with_margin + margin_width,
+						r * court_height_with_margin + margin_height,
+						(Layout.Column - c - 1) * court_width_with_margin + margin_width,
+						(Layout.Row - r - 1) * court_height_with_margin + margin_height);
+					layoutGrid.Children.Add(viewBox);
+				}
+			}
 		}
 	}
 }
