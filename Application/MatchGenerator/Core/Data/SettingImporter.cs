88561f185bb175a8e77bac98090f820c1e8c2f64
Geroshabu <oonstks@gmail.com>
2016-06-19 23:55:54 +0900

    Change class name

--- Application/MatchGenerator/Core/Data/SettingImporter.cs
+++ Application/MatchGenerator/Core/Data/SettingImporter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MatchGenerator.Core.Data
 {
-	class LayoutConfiguration
+	class SettingImporter
 	{
 	}
 }
