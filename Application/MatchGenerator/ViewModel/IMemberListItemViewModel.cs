7147489e99cf053f9254d234f051bc2b0d51a1f9
Geroshabu <oonstks@gmail.com>
2016-09-11 23:04:36 +0900

    refs #18 Define IMemberListItemViewModel interfaces

--- Application/MatchGenerator/ViewModel/IMemberListItemViewModel.cs
+++ Application/MatchGenerator/ViewModel/IMemberListItemViewModel.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace MatchGenerator.ViewModel
 {
+	/// <summary>
+	/// <see cref="System.Windows.Controls.ListView"/>で表示するときの, メンバーのViewModelのインタフェース
+	/// </summary>
 	public interface IMemberListItemViewModel
 	{
+		/// <summary>
+		/// "名前"欄に表示する文字列を取得
+		/// </summary>
+		string Name { get; }
+
+		/// <summary>
+		/// "コメント"欄に表示する文字列を取得
+		/// </summary>
+		string Description { get; }
 	}
 }
