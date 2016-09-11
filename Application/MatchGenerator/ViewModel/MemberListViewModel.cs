00e05baa67227622eed7cd29e21ad1249db7d032
Geroshabu <oonstks@gmail.com>
2016-09-12 00:00:04 +0900

    refs #17 Implement IMemberListViewModel interfaces

--- Application/MatchGenerator/ViewModel/MemberListViewModel.cs
+++ Application/MatchGenerator/ViewModel/MemberListViewModel.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace MatchGenerator.ViewModel
 {
-	internal class MemberListViewModel
+	/// <summary>
+	/// メンバーのリストのViewModel
+	/// </summary>
+	internal class MemberListViewModel : IMemberListViewModel
 	{
+		/// <summary>
+		/// リストに表示するすべてのメンバー
+		/// </summary>
+		public IList<IMemberListItemViewModel> Members
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		/// <summary>
+		/// 現在選択されているメンバー
+		/// </summary>
+		public IList<IMemberListItemViewModel> SelectedMembers
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
 	}
 }
