49f6bee8574fa53fad501dd9122cee239070c841
Geroshabu <oonstks@gmail.com>
2016-09-11 16:24:36 +0900

    refs #9 Add test methods for MemberListItemViewModel members

--- Application/MatchGeneratorTest/ViewModel/MemberListItemViewModelTest.cs
+++ Application/MatchGeneratorTest/ViewModel/MemberListItemViewModelTest.cs
@@ -5,9 +5,30 @@ namespace MatchGeneratorTest.ViewModel
 {
 	public class MemberListItemViewModelTest
 	{
-		[Fact]
-		public void TestMethod1()
+		[Fact(DisplayName = "MemberListItemViewModelコンストラクタ : 正常系", Skip = "未実装")]
+		[Trait("category", "ViewModel")]
+		[Trait("type", "正常系")]
+		public void ConstructorTest()
 		{
+		}
+	}
+
+	public class MemberListItemViewModelInstanceTest
+	{
+		[Fact(DisplayName = "Nameプロパティ : 正常系", Skip = "未実装")]
+		[Trait("category", "ViewModel")]
+		[Trait("type", "正常系")]
+		public void NameTest()
+		{
+
+		}
+
+		[Fact(DisplayName = "Descriptionプロパティ : 正常系", Skip = "未実装")]
+		[Trait("category", "ViewModel")]
+		[Trait("type", "正常系")]
+		public void DescriptionTest()
+		{
+
 		}
 	}
 }
