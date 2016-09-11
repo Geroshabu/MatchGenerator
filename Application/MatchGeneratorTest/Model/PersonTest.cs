9dd277ca5bc4a5b96e37dc6a956e747b2e523a42
Geroshabu <oonstks@gmail.com>
2016-09-11 21:21:18 +0900

    refs #14 Define a mock of Person

--- Application/MatchGeneratorTest/Model/PersonTest.cs
+++ Application/MatchGeneratorTest/Model/PersonTest.cs
@@ -1,5 +1,27 @@
+using System;
+using MatchGenerator.Model;
+
 namespace MatchGeneratorTest.Model
 {
+	internal class PersonMock : IPerson
+	{
+		public string Description
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public string Name
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+	}
+
 	public class PersonTest
 	{
 	}
