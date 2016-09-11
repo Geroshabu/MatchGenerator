5c312c7cbb851c148c1012237c2f42909b0eb726
Geroshabu <oonstks@gmail.com>
2016-09-11 20:56:05 +0900

    refs #12 Define IPerson interfaces

--- Application/MatchGenerator/Model/IPerson.cs
+++ Application/MatchGenerator/Model/IPerson.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace MatchGenerator.Model
 {
+	/// <summary>
+	/// メンバーの情報を保持するModelのインタフェース
+	/// </summary>
 	public interface IPerson
 	{
+		/// <summary>
+		/// このメンバーのお名前
+		/// </summary>
+		string Name { get; }
+
+		/// <summary>
+		/// このメンバーのコメントみたいなやつ. 自由記述のやつ.
+		/// </summary>
+		string Description { get; }
 	}
 }
