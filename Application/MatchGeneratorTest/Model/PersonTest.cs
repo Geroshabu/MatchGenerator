ec696951e79d351a0787ab0283199d975361f488
Geroshabu <oonstks@gmail.com>
2016-09-11 21:27:05 +0900

    refs #14 Implement mock methods

--- Application/MatchGeneratorTest/Model/PersonTest.cs
+++ Application/MatchGeneratorTest/Model/PersonTest.cs
@@ -5,19 +5,25 @@ namespace MatchGeneratorTest.Model
 {
 	internal class PersonMock : IPerson
 	{
+		public Func<string> DescriptionFunc = () => null;
+		public int DescriptionCount = 0;
 		public string Description
 		{
 			get
 			{
-				throw new NotImplementedException();
+				DescriptionCount++;
+				return DescriptionFunc();
 			}
 		}
 
+		public Func<string> NameFunc = () => null;
+		public int NameCount = 0;
 		public string Name
 		{
 			get
 			{
-				throw new NotImplementedException();
+				NameCount++;
+				return NameFunc();
 			}
 		}
 	}
