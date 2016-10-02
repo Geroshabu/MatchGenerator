4fc475781fcc28bab0102ff933de8d8f8929c829
Geroshabu <oonstks@gmail.com>
2016-10-02 13:07:45 +0900

    refs #31 Add class comments

--- Application/MatchGenerator/FileIO/DefaultImporter.cs
+++ Application/MatchGenerator/FileIO/DefaultImporter.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace MatchGenerator.FileIO
 {
+	/// <summary>
+	/// メンバー情報を読み込む標準のインポーター
+	/// </summary>
 	class DefaultImporter
 	{
 	}
--- Application/MatchGenerator/FileIO/IMemberImporter.cs
+++ Application/MatchGenerator/FileIO/IMemberImporter.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace MatchGenerator.FileIO
 {
+	/// <summary>
+	/// メンバー情報をファイルより読み込むインポーターを表す
+	/// </summary>
 	public interface IMemberImporter
 	{
 	}
