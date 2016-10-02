2776b342be972280489a5baca17d0ef383bdc298
Geroshabu <oonstks@gmail.com>
2016-10-02 13:57:02 +0900

    refs #31 Implement the IMemberImporter

--- Application/MatchGenerator/FileIO/DefaultImporter.cs
+++ Application/MatchGenerator/FileIO/DefaultImporter.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MatchGenerator.Model;
 
 namespace MatchGenerator.FileIO
 {
 	/// <summary>
 	/// メンバー情報を読み込む標準のインポーター
 	/// </summary>
-	internal class DefaultImporter
+	internal class DefaultImporter : IMemberImporter
 	{
+		/// <summary>
+		/// メンバー情報を指定したファイルより読み込む
+		/// </summary>
+		/// <param name="FileName">読み込むファイルの, パス(絶対パスまたは相対パス)および拡張子を含むファイル名</param>
+		/// <returns>読み込んだメンバー情報</returns>
+		public IList<IPerson> Import(string FileName)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
