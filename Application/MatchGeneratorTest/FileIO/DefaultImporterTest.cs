c1fb8952d33116b941150d1a8293255f2837b0b2
Geroshabu <oonstks@gmail.com>
2016-10-02 16:22:17 +0900

    refs #31 Create a working directory for tests

--- Application/MatchGeneratorTest/FileIO/DefaultImporterTest.cs
+++ Application/MatchGeneratorTest/FileIO/DefaultImporterTest.cs
@@ -1,12 +1,39 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MatchGenerator.FileIO;
 
 namespace MatchGeneratorTest.FileIO
 {
-	public class DefaultImporterTest
+	public class DefaultImporterTest : IDisposable
 	{
+		private DefaultImporter Instance;
+
+		/// <summary>
+		/// このテストのワーキングディレクトリ
+		/// </summary>
+		private string TestDirectoryPath = $@"{Directory.GetCurrentDirectory()}\{nameof(DefaultImporterTest)}Directory";
+
+		public DefaultImporterTest()
+		{
+			Instance = new DefaultImporter();
+
+			if (Directory.Exists(TestDirectoryPath))
+			{
+				Directory.Delete(TestDirectoryPath, true);
+			}
+			Directory.CreateDirectory(TestDirectoryPath);
+		}
+
+		public void Dispose()
+		{
+			if (Directory.Exists(TestDirectoryPath))
+			{
+				Directory.Delete(TestDirectoryPath, true);
+			}
+		}
 	}
 }
