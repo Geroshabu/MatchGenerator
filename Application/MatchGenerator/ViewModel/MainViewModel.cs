70498b99f2febd2a746db15d12b42d015f60aa1f
Geroshabu <oonstks@gmail.com>
2016-10-09 14:40:36 +0900

    refs #36 Inherit from BindableBase

--- Application/MatchGenerator/ViewModel/MainViewModel.cs
+++ Application/MatchGenerator/ViewModel/MainViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MatchGenerator.ViewModel
 {
-	internal class MainViewModel
+	internal class MainViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
 	{
 	}
 }
