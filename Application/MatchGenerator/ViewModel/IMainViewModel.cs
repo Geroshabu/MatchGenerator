21911f5aeb4b880a7ad076d784368e7556411ed7
Geroshabu <oonstks@gmail.com>
2016-10-09 14:31:05 +0900

    refs #36 Describe the access modifier explicitly

--- Application/MatchGenerator/ViewModel/IMainViewModel.cs
+++ Application/MatchGenerator/ViewModel/IMainViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MatchGenerator.ViewModel
 {
-	interface IMainViewModel
+	public interface IMainViewModel
 	{
 	}
 }
--- Application/MatchGenerator/ViewModel/MainViewModel.cs
+++ Application/MatchGenerator/ViewModel/MainViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MatchGenerator.ViewModel
 {
-	class MainViewModel
+	internal class MainViewModel
 	{
 	}
 }
