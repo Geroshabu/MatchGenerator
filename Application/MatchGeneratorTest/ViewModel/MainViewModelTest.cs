27a3ac30de5b1c40e4873c2e89da7947c083866a
Geroshabu <oonstks@gmail.com>
2016-10-09 14:27:55 +0900

    refs #36 Add new files

---
+++ Application/MatchGeneratorTest/ViewModel/MainViewModelTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MatchGeneratorTest.ViewModel
+{
+	[TestClass]
+	public class MainViewModelTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
