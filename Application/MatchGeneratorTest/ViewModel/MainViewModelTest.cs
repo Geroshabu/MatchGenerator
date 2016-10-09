823e584d2188effb5d1f57a710e24e923fe8455d
Geroshabu <oonstks@gmail.com>
2016-10-09 21:49:04 +0900

    refs #36 Add a test case

--- Application/MatchGeneratorTest/ViewModel/MainViewModelTest.cs
+++ Application/MatchGeneratorTest/ViewModel/MainViewModelTest.cs
@@ -2,6 +2,17 @@ using Xunit;
 
 namespace MatchGeneratorTest.ViewModel
 {
+	public class MainViewModelTest
+	{
+		[Fact(DisplayName = "MainViewModelコンストラクタ : 正常系")]
+		[Trait("category", "ViewModel")]
+		[Trait("type", "正常系")]
+		public void ConstructorTest()
+		{
+
+		}
+	}
+
 	public class MainViewModelInstanceTest
 	{
 		[Fact(DisplayName = "AllMembers.Getterプロパティ : 正常系")]
