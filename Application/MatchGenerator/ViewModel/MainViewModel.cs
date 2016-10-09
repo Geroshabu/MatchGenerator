8fa3c4f8843d8fd764e8b00d2afeb1e436b8b86f
Geroshabu <oonstks@gmail.com>
2016-10-09 14:41:29 +0900

    refs #36 Implement the IMainViewModel interface

--- Application/MatchGenerator/ViewModel/MainViewModel.cs
+++ Application/MatchGenerator/ViewModel/MainViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MatchGenerator.ViewModel
 {
-	internal class MainViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+	internal class MainViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase, IMainViewModel
 	{
 	}
 }
