8bd5c4c19486dd1d15b69c61b04684b154a7b1ae
Geroshabu <oonstks@gmail.com>
2016-10-16 21:24:20 +0900

    refs #56 Add a comment

--- Application/MatchGenerator/ViewModel/IViewModelEquatable.cs
+++ Application/MatchGenerator/ViewModel/IViewModelEquatable.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace MatchGenerator.ViewModel
 {
+	/// <summary>
+	/// ViewModelとして同一であるかを判定できることを表す.
+	/// </summary>
 	public interface IViewModelEquatable
 	{
 	}
