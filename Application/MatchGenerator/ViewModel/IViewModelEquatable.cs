c91f0eb9a35b82594ed2e406fc1c9c10a2dcf90d
Geroshabu <oonstks@gmail.com>
2016-10-16 21:31:09 +0900

    refs #56 Add an interface to judge whether two ViewModels are equal

--- Application/MatchGenerator/ViewModel/IViewModelEquatable.cs
+++ Application/MatchGenerator/ViewModel/IViewModelEquatable.cs
@@ -11,5 +11,16 @@ namespace MatchGenerator.ViewModel
 	/// </summary>
 	public interface IViewModelEquatable
 	{
+		/// <summary>
+		/// このViewModelが表現しているModelが,
+		/// 与えられたViewModelが表現しているModelと
+		/// 等しいかどうかを判定する.
+		/// </summary>
+		/// <param name="other">このViewModelと比較するViewModel</param>
+		/// <returns>このViewModelが表現しているModelが,
+		/// 与えられたViewModelが表現しているModelと
+		/// 等しいかどうか.
+		/// 等しければtrue, 等しくなければfalse</returns>
+		bool EqualsModel(IViewModelEquatable other);
 	}
 }
