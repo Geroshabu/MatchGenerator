2581b39d97c3e0ec12c97f3901a8ba30f0005418
Geroshabu <oonstks@gmail.com>
2016-10-17 21:36:09 +0900

    refs #59 Add a property to get the model

--- Application/MatchGenerator/ViewModel/IViewModel.cs
+++ Application/MatchGenerator/ViewModel/IViewModel.cs
@@ -12,5 +12,9 @@ namespace MatchGenerator.ViewModel
 	/// <typeparam name="T">このViewModelが表現するModelの型</typeparam>
 	public interface IViewModel<T>
 	{
+		/// <summary>
+		/// このViewModelが表現するModelを取得する.
+		/// </summary>
+		T Model { get; }
 	}
 }
