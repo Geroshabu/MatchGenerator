5751e78d38d529aa3c3746b10ba8a5aafd1d9572
Geroshabu <oonstks@gmail.com>
2017-04-16 01:43:18 +0900

    refs #116 Inherit MefBootstrapper

--- Application/MatchGeneratorTest/Bootstrapper.cs
+++ Application/MatchGeneratorTest/Bootstrapper.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Prism.Mef;
 
 namespace MatchGeneratorTest
 {
-	internal class Bootstrapper
+	internal class Bootstrapper : MefBootstrapper
 	{
 	}
 }
